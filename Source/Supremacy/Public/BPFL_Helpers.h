92f6e72b076da3034e55502b486da8059a059053
Torrunt <me@torrunt.net>
2021-12-09 10:44:00 +0800

    Parse incoming net messages + User Action Manager

--- Source/Supremacy/Public/BPFL_Helpers.h
+++ Source/Supremacy/Public/BPFL_Helpers.h
@@ -13,10 +13,18 @@ UCLASS()
 class SUPREMACY_API UBPFL_Helpers : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-
+	
 public:
 
+	/** Parses  */
+	UFUNCTION(BlueprintPure, Category = "Helpers", DisplayName = "Parse Net Message", meta=(Keywords = "Parse Net Message Bytes Byte Array"))
+	static void ParseNetMessage(const TArray<uint8> Bytes, uint8 &Type, FString &Message);
+	
 	/** Converts a String to an Array of bytes */
 	UFUNCTION(BlueprintPure, Category = "Helpers", DisplayName = "Convert String To Bytes")
 	static void ConvertStringToBytes(const FString String, TArray<uint8> &Bytes);
+	
+	/** Converts an Array of bytes to a String */
+	UFUNCTION(BlueprintPure, Category = "Helpers", DisplayName = "Convert Bytes To String")
+	static void ConvertBytesToString(const TArray<uint8> Bytes, FString &String);
 };
