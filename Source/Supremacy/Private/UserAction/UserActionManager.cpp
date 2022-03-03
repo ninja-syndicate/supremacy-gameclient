6a4b6c810770e547fce6318ca0d220c0bd7dbb33
kimaust <59196774+kimaust@users.noreply.github.com>
2022-03-03 16:59:52 +1100

    FIX - User action queue (#117)
    * Renamed previous UserActionManager to BP_UserActionManager
    
    * Changed navigation generation to static because dynamic isn't needed yet and increased max crowd agents to 80
    
    * Resaved the file to prevent failed-to-save issue
    
    * Fixed typo
    
    * C++ UserActionManager base with queue implementation and struct for the user action
    
    * Fixed redirects after renaming
    
    * Fixed spawn satellite error
    
    * Fixed redirect
    
    * Fixed merge conflicts

---
+++ Source/Supremacy/Private/UserAction/UserActionManager.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UserAction/UserActionManager.h"
+
+// Sets default values for this component's properties
+UUserActionManager::UUserActionManager()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	
+	// ...
+}
+
+
+// Called when the game starts
+void UUserActionManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UUserActionManager::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+bool UUserActionManager::IsQueueEmpty()
+{
+	return UserActionQueue.IsEmpty();
+}
+
+void UUserActionManager::EnqueueUserAction(const FUserAction& UserAction)
+{
+	UserActionQueue.Enqueue(UserAction);
+}
+
+bool UUserActionManager::DequeueUserAction(FUserAction& OutUserAction)
+{
+	return UserActionQueue.Dequeue(OutUserAction);
+}
