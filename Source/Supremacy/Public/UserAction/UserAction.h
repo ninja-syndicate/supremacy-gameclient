6b88fec2cae29a20fcf2af59b93824077f14ee22
Minsoo Kim <kim.aus@outlook.com>
2022-03-03 09:33:26 +0800

    C++ UserActionManager base with queue implementation and struct for the user action

---
+++ Source/Supremacy/Public/UserAction/UserAction.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UserAction.generated.h"
+
+USTRUCT(BlueprintType)
+struct FUserAction
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * The ability ID associated with this user action.
+	 *
+	 * @note For now, this is stored as an integer as we have enum in blueprint.
+	 * We probably should make a C++ AbilityID enum and switch to it eventually.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int AbilityId;
+
+	/** The 2D target location of the user action. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector2D Location;
+
+	/** The user action event ID. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString EventId;
+
+	/** The username of who triggered this user action. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString TriggeredByUsername;
+
+	/** War machine hash. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString WarMachineHash;
+};
