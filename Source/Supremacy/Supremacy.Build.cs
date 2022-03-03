ed9e2a47f6609ba0ebbfcd771b21cde035b4fb11
Minsoo Kim <kim.aus@outlook.com>
2022-03-03 09:32:51 +0800

    Fixed typo

--- Source/Supremacy/Supremacy.Build.cs
+++ Source/Supremacy/Supremacy.Build.cs
@@ -8,7 +8,7 @@ public class Supremacy : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GamePlayTasks", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTasks", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
