440bb5af001cb720f431de2b248c289b4d75ca87
Minsoo Kim <kim.aus@outlook.com>
2022-07-12 11:43:20 +0800

    Updated threshold

--- Content/Scripts/default-mech-ai/src/predicates/Predicate_HasLowShield.ts
+++ Content/Scripts/default-mech-ai/src/predicates/Predicate_HasLowShield.ts
@@ -4,9 +4,9 @@ import { AIBlackboard } from "@blackboards/blackboard"
  * Checks if AI has low shield.
  *
  * @param blackboard
- * @returns true if the percentage of current shield to shield max is <= 30% and false otherwise
+ * @returns true if the percentage of current shield to shield max is <= 40% and false otherwise
  */
 export const HasLowShield = (blackboard: AIBlackboard) => {
     let self = blackboard.input.self
-    return self.shield / self.shieldMax <= 0.3
+    return self.shield / self.shieldMax <= 0.4
 }
