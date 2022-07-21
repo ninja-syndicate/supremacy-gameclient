4b3987f817df9734d813dddbf949011dbc1f98f1
Minsoo Kim <kim.aus@outlook.com>
2022-07-21 10:28:29 +0800

    Made changes to AI to work without battle zone.

--- Content/Scripts/default-mech-ai/src/types/types.d.ts
+++ Content/Scripts/default-mech-ai/src/types/types.d.ts
@@ -160,8 +160,9 @@ declare class AIController {
      * Get results from EQS in {@link BrainInput}.
      *
      * @param query The environment query type (@see {@link EQSQueryType})
+     * @returns true if the specified EQS query succeeded and false otherwise.
      */
-    EQS_Query(query: EQSQueryType): void
+    EQS_Query(query: EQSQueryType): boolean
 
     /**
      * Removes EQS query status from {@link BrainInput.eqs}, essentially marking it as complete so you know you can run it again.
--- Content/Scripts/default-mech-ai/src/utils.ts
+++ Content/Scripts/default-mech-ai/src/utils.ts
@@ -1,25 +1,29 @@
-import { EQSQueryType } from "enums";
-import { Vector, WarMachine } from "types";
+import { EQSQueryType } from "enums"
+import { Vector, WarMachine } from "types"
 
 export const StringToEQSQueryType = (key: string) => {
     switch (key) {
         case "away":
-            return EQSQueryType.Away;
+            return EQSQueryType.Away
         case "cover":
-            return EQSQueryType.Cover;
+            return EQSQueryType.Cover
         case "hidden":
-            return EQSQueryType.Hidden;
+            return EQSQueryType.Hidden
         case "patrol":
-            return EQSQueryType.Patrol;
+            return EQSQueryType.Patrol
         case "strafe":
-            return EQSQueryType.Strafe;
+            return EQSQueryType.Strafe
+        case "closeStrafe":
+            return EQSQueryType.CloseStrafe
+        case "battleZone":
+            return EQSQueryType.BattleZone
     }
 }
 
 export const IsVector = (object: any): object is Vector => {
-    return 'X' in object && 'Y' in object;
+    return "X" in object && "Y" in object
 }
 
 export const IsWarMachine = (object: any): object is WarMachine => {
-    return 'hash' in object;
+    return "hash" in object
 }
