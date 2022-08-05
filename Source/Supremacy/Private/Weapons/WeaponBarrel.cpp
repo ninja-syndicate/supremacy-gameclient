1de1c37c5865b0442a08b0044d3c9f4c7a38fa23
Torrunt <me@torrunt.net>
2022-08-05 15:22:27 +0800

    Started new WeaponBarrel class

---
+++ Source/Supremacy/Private/Weapons/WeaponBarrel.cpp
@@ -0,0 +1,243 @@
+﻿#include "Weapons/WeaponBarrel.h"
+#include "EBBullet.h"
+
+UWeaponBarrel::UWeaponBarrel()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	bHiddenInGame = true;
+	bAutoActivate = true;
+	SetIsReplicatedByDefault(ReplicateVariables);
+
+	RandomStream.GenerateNewSeed();
+}
+
+void UWeaponBarrel::TickComponent(const float DeltaTime, const ELevelTick TickType,
+                                  FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (ClientSideAim)
+	{
+		if (GetOwner()->GetRemoteRole() == ROLE_Authority)
+		{
+			TimeSinceAimUpdate += DeltaTime;
+
+			if (TimeSinceAimUpdate >= 1.0f / ClientAimUpdateFrequency)
+			{
+				Aim = GetComponentTransform().GetUnitAxis(EAxis::X);
+				Location = GetComponentTransform().GetLocation();
+				ClientAim(UGameplayStatics::RebaseLocalOriginOntoZero(GetWorld(), Location), Aim);
+				TimeSinceAimUpdate = FMath::Fmod(TimeSinceAimUpdate, 1.0f / ClientAimUpdateFrequency);
+			};
+		}
+		else
+		{
+			if (!RemoteAimReceived)
+			{
+				Aim = GetComponentTransform().GetUnitAxis(EAxis::X);
+				Location = GetComponentTransform().GetLocation();
+			}
+			else
+			{
+				if (const FVector LocOffset = (Location - GetComponentLocation()); LocOffset.Size() >
+					ClientAimDistanceLimit)
+				{
+					//lag or cheater???
+					Location = GetComponentLocation() + LocOffset.GetSafeNormal() * ClientAimDistanceLimit;
+				}
+			}
+		}
+	}
+	else
+	{
+		Aim = GetComponentTransform().GetUnitAxis(EAxis::X);
+		Location = GetComponentTransform().GetLocation();
+	}
+
+	//Only server can tick
+	if (GetOwner()->GetLocalRole() == ROLE_Authority)
+	{
+		float RemainingDelta = DeltaTime;
+
+		do
+		{
+			const float Step = FMath::Min(Cooldown, RemainingDelta);
+
+			Cooldown -= Step;
+
+			RemainingDelta -= Step;
+
+			//shoot when ready
+			if (Shooting && (!ShootingBlocked))
+			{
+				if (ProjectileAmount > 0 &&  BurstFireRate == 0)
+				{
+					// Shotgun
+					for (int i = 0; i < ProjectileAmount; i++)
+					{
+						SpawnBullet(GetOwner(), Location, Aim);
+					}
+				}
+				else
+				{
+					SpawnBullet(GetOwner(), Location, Aim);
+				}
+			}
+		}
+		while (RemainingDelta > 0 && Cooldown > 0);
+	}
+}
+
+void UWeaponBarrel::SpawnBullet(AActor* Owner, const FVector LocalLocation, const FVector LocalAim)
+{
+	FVector OutLocation;
+	FVector OutAim;
+
+	InitialBulletTransform(LocalLocation, LocalAim, OutLocation, OutAim);
+
+	const AEBBullet* Default = Cast<AEBBullet>(Ammo->GetDefaultObject());
+
+	float BarrelSpread = Spread;
+	if (SpreadBias > 0.0f)
+	{
+		const float SpreadMulti = FMath::Pow(FMath::FRand(), SpreadBias);
+		BarrelSpread *= SpreadMulti;
+	}
+
+	const float TotalSpread = BarrelSpread;
+
+	OutAim = RandomStream.VRandCone(OutAim, TotalSpread);
+	FVector Velocity = OutAim * MuzzleVelocityMultiplier;
+
+	//get parent physics body
+	UPrimitiveComponent* Parent = Cast<UPrimitiveComponent>(GetAttachParent());
+	Velocity += AdditionalVelocity;
+
+	if (Parent != nullptr)
+	{
+		if (Parent->IsSimulatingPhysics())
+		{
+			Velocity += Parent->GetPhysicsLinearVelocityAtPoint(OutLocation) * InheritVelocity;
+		}
+
+		ApplyRecoil(Parent, OutLocation, -Velocity * Default->Mass * RecoilMultiplier);
+	}
+	
+	//UGameplayStatics::SuggestProjectileVelocity_CustomArc(GetWorld(), Velocity, LocalLocation, )
+
+	BeforeShotFired.Broadcast();
+
+	AEBBullet::SpawnWithExactVelocity(Ammo, Owner, Owner->GetInstigator(), OutLocation, Velocity);
+
+	Cooldown = 60.0f / FireRate;
+	
+	if (ReplicateShotFiredEvents)
+	{
+		ShotFiredMulticast();
+	}
+	else
+	{
+		ShotFired.Broadcast();
+	}
+}
+
+void UWeaponBarrel::InitialBulletTransform_Implementation(const FVector InLocation, const FVector InDirection, FVector& OutLocation,
+                                                          FVector& OutDirection)
+{
+	OutLocation = InLocation;
+	OutDirection = InDirection;
+}
+
+void UWeaponBarrel::ApplyRecoil_Implementation(UPrimitiveComponent* Component, const FVector InLocation, const FVector Impulse)
+{
+	if (Component->IsSimulatingPhysics())
+	{
+		Component->AddImpulseAtLocation(Impulse, InLocation);
+	}
+}
+
+void UWeaponBarrel::CalculateAimDirection(TSubclassOf<class AEBBullet> BulletClass, FVector TargetLocation,
+                                          FVector TargetVelocity, FVector& AimDirection,
+                                          FVector& PredictedTargetLocation, FVector& PredictedIntersectionLocation,
+                                          float& PredictedFlightTime, float& Error, float MaxTime, float Step,
+                                          int NumIterations) const
+{
+	const FVector StartLocation = GetComponentLocation();
+	CalculateAimDirectionFromLocation(BulletClass, StartLocation, TargetLocation, TargetVelocity, AimDirection,
+	                                  PredictedTargetLocation, PredictedIntersectionLocation, PredictedFlightTime,
+	                                  Error, MaxTime, Step, NumIterations);
+}
+
+void UWeaponBarrel::CalculateAimDirectionFromLocation(TSubclassOf<class AEBBullet> BulletClass, FVector StartLocation,
+                                                      FVector TargetLocation, FVector TargetVelocity,
+                                                      FVector& AimDirection, FVector& PredictedTargetLocation,
+                                                      FVector& PredictedIntersectionLocation,
+                                                      float& PredictedFlightTime, float& Error, float MaxTime,
+                                                      float Step, int NumIterations) const
+{
+	if (!BulletClass->IsValidLowLevel())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CalculateAimDirection - invalid bullet class"));
+		return;
+	}
+
+	AEBBullet* Bullet = Cast<AEBBullet>(BulletClass->GetDefaultObject());
+
+	FVector AddVelocity = AdditionalVelocity;
+	if (UPrimitiveComponent* Parent = Cast<UPrimitiveComponent>(GetAttachParent()); Parent != nullptr)
+	{
+		if (Parent->IsSimulatingPhysics())
+		{
+			AddVelocity += Parent->GetPhysicsLinearVelocityAtPoint(StartLocation) * InheritVelocity;
+		}
+	}
+
+
+	FVector InitialAimDirection = (TargetLocation - StartLocation).GetSafeNormal(); //initial prediction
+	AimDirection = InitialAimDirection;
+
+	for (int Iteration = 0; Iteration < NumIterations; Iteration++)
+	{
+		FVector CurrentBulletLocation = StartLocation;
+		FVector Velocity = (AimDirection * (MuzzleVelocityMultiplier * FMath::Lerp(
+			Bullet->MuzzleVelocityMin, Bullet->MuzzleVelocityMax, 0.5))) + AddVelocity;
+		bool bHit = false;
+		for (float Time = 0; Time <= MaxTime; Time += Step)
+		{
+			FVector PreviousVelocity = Velocity;
+			Velocity = Bullet->UpdateVelocity(GetWorld(), CurrentBulletLocation, Velocity, Step);
+
+			FVector TraceVector = ((((PreviousVelocity + Velocity) * 0.5) - TargetVelocity) * Step);
+			FVector TraceEndLocation = CurrentBulletLocation + TraceVector;
+			FVector IntersectionPoint;
+
+			bHit = FMath::SegmentPlaneIntersection(CurrentBulletLocation - TraceVector, TraceEndLocation,
+			                                       FPlane(TargetLocation, InitialAimDirection), IntersectionPoint);
+			//actual hit test
+
+			if (bHit)
+			{
+				PredictedIntersectionLocation = IntersectionPoint;
+				FQuat AimCorrection = FQuat::FindBetween((IntersectionPoint - StartLocation),
+				                                         (TargetLocation - StartLocation));
+				AimDirection = AimCorrection.RotateVector(AimDirection).GetSafeNormal();
+				Error = (IntersectionPoint - TargetLocation).Size();
+
+				float AdditionalFlightTime = (FVector(CurrentBulletLocation - IntersectionPoint).Size() / TraceVector.
+					Size()) * Step;
+				PredictedFlightTime = Time + AdditionalFlightTime;
+				PredictedTargetLocation = TargetLocation + TargetVelocity * AdditionalFlightTime;
+
+				break;
+			}
+
+			//no hit, keep going
+			CurrentBulletLocation = TraceEndLocation;
+		}
+		if (!bHit)
+		{
+			Error = 99999999999999999.0f;
+			return; //no solution
+		}
+	}
+}
---
+++ Source/Supremacy/Public/Weapons/WeaponBarrel.h
@@ -0,0 +1,99 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/PrimitiveComponent.h"
+#include "WeaponBarrel.generated.h"
+
+UCLASS(Blueprintable, ClassGroup = (Custom), hidecategories = (Object, LOD, Physics, Lighting, TextureStreaming, Collision, HLOD, Mobile, VirtualTexture, ComponentReplication), editinlinenew, meta = (BlueprintSpawnableComponent))
+	class SUPREMACY_API UWeaponBarrel final : public UPrimitiveComponent
+{
+
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UWeaponBarrel();
+
+	// Called every frame
+	virtual void TickComponent(const float DeltaTime, const ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Debug") float DebugArrowSize = 100.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Velocity", meta = (ToolTip = "Bullet inherits barrel velocity, only works with physics enabled or with additional velocity set")) float InheritVelocity = 1.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Velocity", meta = (ToolTip = "Amount of recoil applied to the barrel, only works with physics enabled")) float RecoilMultiplier = 1.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Velocity", meta = (ToolTip = "Additional velocity, for use with InheritVelocity")) FVector AdditionalVelocity = FVector(0,0,0);
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Weapon", meta = (ToolTip = "Additional maximum spread, in radians, applied on top of bullet spread", ClampMin = "0")) float Spread=0.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Weapon", meta = (ToolTip = "Additional Spread bias, higher is more accurate on average", ClampMin = "0")) float SpreadBias = 0.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Weapon", meta = (ToolTip = "Multiplier applied to bullet muzzle velocity")) float MuzzleVelocityMultiplier = 1.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Weapon", meta = (ToolTip = "Fire rate, rounds per minute")) float FireRate = 60.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Weapon", meta = (ToolTip = "Fire rate inside a burst, rounds per minute. 0 = Non-burst weapon.")) float BurstFireRate = 0.0f;
+	UPROPERTY(Replicated, BlueprintReadWrite, EditAnywhere, Category = "Weapon") bool ShootingBlocked;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Arced Firing", meta = (ToolTip = "If true; projectiles will arc towards TargetLocation.")) bool IsArced = false;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Arced Firing", meta = (ToolTip = "Optional override of WorldGravityZ for calculating arc. If 0; will use projectile's gravity.")) float ArcOverrideGravityZ = 0;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Arced Firing", meta = (ToolTip = "Change height of arc between 0.0-1.0 where 0.5 is the default medium arc, 0 is up, and 1 is directly toward TargetLocation.")) float ArcParam = 0.5;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Weapon", meta = (ToolTip = "Number of projectiles spawned per shot")) int ProjectileAmount = 1;
+
+	UPROPERTY(Replicated, BlueprintReadWrite, EditAnywhere, Category = "Ammo") TSubclassOf<class AEBBullet> Ammo;
+
+	UPROPERTY(Replicated, BlueprintReadWrite, Category = "WeaponState") bool Shooting;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "WeaponState") float Cooldown;
+	UPROPERTY(BlueprintReadWrite, Category = "WeaponState") float BurstCooldown;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Replication") bool ReplicateVariables=true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Replication") bool ReplicateShotFiredEvents = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Replication") bool ClientSideAim=false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Replication") float ClientAimUpdateFrequency = 15.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Replication") float ClientAimDistanceLimit = 200.0f;
+
+	FRandomStream RandomStream;
+
+	UFUNCTION(BlueprintCallable, Category = "Shooting") void Shoot(const bool Trigger);
+
+	UFUNCTION(BlueprintCallable, meta = (AutoCreateRefTerm = "IgnoredActors"), Category = "Prediction") void PredictHit(bool& Hit, FHitResult& HitResult, FVector& HitLocation, float& HitTime, AActor*& HitActor, TArray<FVector>& Trajectory, const TSubclassOf<class AEBBullet> BulletClass, const TArray<AActor*>IgnoredActors, const float MaxTime = 10.0f, const float Step = 0.1f) const;
+	UFUNCTION(BlueprintCallable, meta = (AutoCreateRefTerm = "IgnoredActors"), Category = "Prediction") void PredictHitFromLocation(bool &Hit, FHitResult& TraceResult, FVector& HitLocation, float& HitTime, AActor*& HitActor, TArray<FVector>& Trajectory, const TSubclassOf<class AEBBullet> BulletClass, const FVector StartLocation, const FVector AimDirection, const TArray<AActor*>IgnoredActors, const float MaxTime = 10.0f, const float Step = 0.1f) const;
+	UFUNCTION(BlueprintCallable, Category = "Prediction") void CalculateAimDirection(TSubclassOf<class AEBBullet> BulletClass, FVector TargetLocation, FVector TargetVelocity, FVector& AimDirection, FVector& PredictedTargetLocation, FVector& PredictedIntersectionLocation, float& PredictedFlightTime, float& Error, float MaxTime = 10.0f, float Step = 0.1f, int NumIterations = 4) const;
+	UFUNCTION(BlueprintCallable, Category = "Prediction") void CalculateAimDirectionFromLocation(TSubclassOf<class AEBBullet> BulletClass, FVector StartLocation, FVector TargetLocation, FVector TargetVelocity, FVector& AimDirection, FVector& PredictedTargetLocation, FVector& PredictedIntersectionLocation, float& PredictedFlightTime, float& Error, float MaxTime = 10.0f, float Step=0.1f, int NumIterations = 4) const;
+	
+	UFUNCTION(BlueprintNativeEvent, Category = "Events") void InitialBulletTransform(const FVector InLocation, const FVector InDirection, FVector& OutLocation, FVector& OutDirection);
+	UFUNCTION(BlueprintNativeEvent, Category = "Events") void ApplyRecoil(UPrimitiveComponent* Component, const FVector InLocation, const FVector Impulse);
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FBeforeShotFired);
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+		FBeforeShotFired BeforeShotFired;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FShotFired);
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+		FShotFired ShotFired;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FAmmoDepleted);
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+		FAmmoDepleted AmmoDepleted;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FReadyToShoot);
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+		FReadyToShoot ReadyToShoot;
+
+#ifdef WITH_EDITOR
+	virtual FPrimitiveSceneProxy* CreateSceneProxy() override;
+#endif
+
+private:
+	void SpawnBullet(AActor* Owner, const FVector LocalLocation, const FVector LocalAim);
+
+	UFUNCTION(Server, Unreliable, WithValidation) void ClientAim(const FVector_NetQuantize NewLocation, const FVector_NetQuantizeNormal NewAim);
+	UFUNCTION(Server, Reliable, WithValidation) void ShootRep(const bool Trigger);
+	UFUNCTION(Server, Reliable, WithValidation) void ShootRepCSA(const bool Trigger, const FVector_NetQuantize NewLocation, const FVector_NetQuantizeNormal NewAim);
+
+	UFUNCTION(NetMulticast, Reliable)
+		void ShotFiredMulticast();
+
+	FVector Aim;
+	FVector Location;
+	bool RemoteAimReceived;
+	float TimeSinceAimUpdate;
+	bool PredictTrace(UWorld* World, AEBBullet* Bullet, FVector Start, FVector End, FHitResult &HitResult, TArray<AActor*> IgnoredActors) const;
+};
