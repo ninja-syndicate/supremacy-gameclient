0424aa0d7ee7061a8566cbf6ff121e9bc1a848ef
Minsoo Kim <kim.aus@outlook.com>
2022-08-08 11:16:00 +0800

    Initial focus to gunshot WIP. TODO: Implementation

---
+++ Content/Scripts/default-mech-ai/src/predicates/Predicate_FocusToGunshot.ts
@@ -0,0 +1,12 @@
+import { AIBlackboard } from "@blackboards/blackboard"
+
+export const Predicate_FocusToGunshot = (blackboard: AIBlackboard): boolean => {
+    // Check undefines.
+    if (typeof blackboard.targetPredictedLocation === "undefined") return false
+
+    // TODO: Provide the time-limit as a globally configurable value.
+    const timeDiff: number = blackboard.currentTime - blackboard.targetLostSightTime
+    if (timeDiff >= 10) return false
+
+    return true
+}
