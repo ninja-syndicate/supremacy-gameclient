80641d146a22ac1db76e4611b729f3909995310e
Minsoo Kim <kim.aus@outlook.com>
2022-08-15 14:06:10 +0800

    Added more options to ai config

--- Content/Scripts/default-mech-ai/src/aiconfig.ts
+++ Content/Scripts/default-mech-ai/src/aiconfig.ts
@@ -8,10 +8,14 @@ interface AIConfig {
     closeCombatEnterRange: number
     /** The range at which AI will exit the close combat behavior. Must be larger than {@link closeCombatEnterRange} to work properly. */
     closeCombatExitRange: number
+    /** The threshold used for AI to think it has low shield. Must be in the range [0, 1]. */
+    lowShieldThreshold: number
     /** The threshold used for AI to think it's in very low total health. Must be in the range [0, 1]. */
     veryLowTotalHealthThreshold: number
     /** The number of seconds a new damage stimulus will be remembered. NOTE: May end up keeping an array, so this may be removed in the future. */
     damageStimulusTimeout: number
+    /** The maximum number of seconds to predict the target location before giving up. */
+    predictionTimeout: number
     /** TODO: Implementation. This value does nothing currently. */
     tauntNoiseStimulusTimeout: number
     /** The number of seconds a new weapon noise will be remembered. NOTE: May end up keeping an array, so this may be removed in the future. */
@@ -20,33 +24,41 @@ interface AIConfig {
 
 /** Default AI configuration. */
 const DEFAULT_AI_CONFIG: AIConfig = {
-    closeCombatEnterRange: 3000,
-    closeCombatExitRange: 5000,
-    veryLowTotalHealthThreshold: 0.4,
+    closeCombatEnterRange: 2250,
+    closeCombatExitRange: 3500,
+    lowShieldThreshold: 0.45,
+    veryLowTotalHealthThreshold: 0.45,
     damageStimulusTimeout: 10,
+    predictionTimeout: 10,
     tauntNoiseStimulusTimeout: 30,
     weaponNoiseStimulusTimeout: 5
+
 }
 
 /** Aggressive AI configuration. */
 const AGGRESSIVE_AI_CONFIG: AIConfig = {
-    closeCombatEnterRange: 5000,
-    closeCombatExitRange: 6000,
+    closeCombatEnterRange: 2250,
+    closeCombatExitRange: 3500,
+    lowShieldThreshold: 0.2,
     veryLowTotalHealthThreshold: 0.2,
     damageStimulusTimeout: 10,
+    predictionTimeout: 10,
     tauntNoiseStimulusTimeout: 30,
     weaponNoiseStimulusTimeout: 5
 }
 
 /** Conservative AI configuration. */
 const CONSERVATIVE_AI_CONFIG: AIConfig = {
-    closeCombatEnterRange: 2000,
-    closeCombatExitRange: 3000,
+    closeCombatEnterRange: 2250,
+    closeCombatExitRange: 3500,
+    lowShieldThreshold: 0.6,
     veryLowTotalHealthThreshold: 0.6,
     damageStimulusTimeout: 10,
+    predictionTimeout: 10,
     tauntNoiseStimulusTimeout: 30,
     weaponNoiseStimulusTimeout: 5
 }
 
-/** Change this to the desired AI config preset. */
+/** Current AI configuration. Change this to the desired AI config preset. */
 export const CURRENT_AI_CONFIG: AIConfig = DEFAULT_AI_CONFIG
+export const ENABLE_DEBUGGING: boolean = false
