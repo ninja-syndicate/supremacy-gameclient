56f57cb9c21e3eaed691e99bad46b782bb266516
Minsoo Kim <kim.aus@outlook.com>
2022-08-15 13:50:32 +0800

    Made it use ai config values

--- Content/Scripts/default-mech-ai/src/predicates/Predicate_HasLowShield.ts
+++ Content/Scripts/default-mech-ai/src/predicates/Predicate_HasLowShield.ts
@@ -1,12 +1,13 @@
 import { AIBlackboard } from "@blackboards/blackboard"
+import { CURRENT_AI_CONFIG } from "@root/aiconfig"
 
 /**
  * Checks if AI has low shield.
  *
  * @param blackboard
- * @returns true if the percentage of current shield to shield max is <= 40% and false otherwise
+ * @returns true if the percentage of current shield to shield max is <= {@link CURRENT_AI_CONFIG.lowShieldThreshold} and false otherwise
  */
 export const HasLowShield = (blackboard: AIBlackboard) => {
     let self = blackboard.input.self
-    return self.shield / self.shieldMax <= 0.4
+    return self.shield / self.shieldMax <= CURRENT_AI_CONFIG.lowShieldThreshold
 }
