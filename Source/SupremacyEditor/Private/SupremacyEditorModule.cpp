18dfef63a7f399527f96cb9f0baf21bdf40bac5e
Brendan Ragan <lordmortis@gmail.com>
2022-08-15 16:41:18 +0800

    Basic modules stuff

---
+++ Source/SupremacyEditor/Private/SupremacyEditorModule.cpp
@@ -0,0 +1,5 @@
+#include "SupremacyEditorModule.h"
+#include "Modules/ModuleManager.h"
+#include "Modules/ModuleInterface.h"
+
+IMPLEMENT_GAME_MODULE(FSupremacyEditorModule, MyGameEditor);
---
+++ Source/SupremacyEditor/Public/SupremacyEditorModule.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Engine.h"
+#include "Modules/ModuleInterface.h"
+#include "Modules/ModuleManager.h"
+#include "UnrealEd.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(SupremacyEditor, All, All)
+
+class FSupremacyEditorModule: public IModuleInterface
+{
+};
