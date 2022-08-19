e0241dff3517fb46435a09613c686fff7db3cece
Minsoo Kim <kim.aus@outlook.com>
2022-08-19 14:25:55 +0800

    Added user action support

--- Content/Scripts/default-mech-ai/src/predicates/Predicate_CanActivateAction.ts
+++ Content/Scripts/default-mech-ai/src/predicates/Predicate_CanActivateAction.ts
@@ -1,7 +1,7 @@
 import { AIBlackboard } from "@blackboards/blackboard"
-import { Action } from "enums"
+import { Action, UserAction } from "enums"
 import { AI } from "@root/index"
 
-export const Predicate_CanActivateAction = (action: Action) => (blackboard: AIBlackboard) => {
+export const Predicate_CanActivateAction = (action: Action | UserAction) => (blackboard: AIBlackboard) => {
     return !!AI.CanActivateAction(action)
 }
