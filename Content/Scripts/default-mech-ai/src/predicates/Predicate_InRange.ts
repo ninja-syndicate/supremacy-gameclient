6c24156c04c50229b81c0b2931e576c74405efec
Minsoo Kim <kim.aus@outlook.com>
2022-08-19 14:26:11 +0800

    renamed

--- Content/Scripts/default-mech-ai/src/predicates/Predicate_InRange.ts
+++ Content/Scripts/default-mech-ai/src/predicates/Predicate_InRange.ts
@@ -18,7 +18,7 @@ export const InRange = (v1: Vector, v2: Vector, range: number) => (blackboard: A
  * @param range the range to check for
  * @returns true if the distance to target is within the range and false otherwise.
  */
-export const TargetInRange = (range: number) => (blackboard: AIBlackboard) => {
+export const Predicate_TargetInRange = (range: number) => (blackboard: AIBlackboard) => {
     if (blackboard.target === null) return false
 
     return InRange(blackboard.input.self.location, blackboard.target.location, range)(blackboard)
