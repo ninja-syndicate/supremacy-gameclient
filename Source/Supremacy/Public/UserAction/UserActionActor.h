805d0d734eab19e4f413b9fbad1fefe4a7db710c
Torrunt <me@torrunt.net>
2022-08-22 11:58:23 +0800

    Shield, Utility and Drone hard ref fixes

---
+++ Source/Supremacy/Public/UserAction/UserActionActor.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "UserActionActor.generated.h"
+
+UCLASS()
+class SUPREMACY_API AUserActionActor final : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	/** The user action event ID. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ExposeOnSpawn="true"))
+	FString EventId;
+
+	/** The username of who triggered this user action. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ExposeOnSpawn="true"))
+	FString TriggeredByUsername;
+
+	/** War machine hash. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ExposeOnSpawn="true"))
+	FString WarMachineHash;
+};
