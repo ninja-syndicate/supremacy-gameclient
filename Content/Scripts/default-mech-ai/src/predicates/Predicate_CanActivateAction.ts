9198f468bac2f47686cbaa53b7c40997dbd73605
Minsoo Kim <kim.aus@outlook.com>
2022-08-23 10:25:56 +0800

    Added user action variant of can activate action to easily disable it

--- Content/Scripts/default-mech-ai/src/predicates/Predicate_CanActivateAction.ts
+++ Content/Scripts/default-mech-ai/src/predicates/Predicate_CanActivateAction.ts
@@ -1,7 +1,17 @@
 import { AIBlackboard } from "@blackboards/blackboard"
 import { Action, UserAction } from "enums"
 import { AI } from "@root/index"
+import { CURRENT_AI_CONFIG } from "@root/aiconfig"
 
 export const Predicate_CanActivateAction = (action: Action | UserAction) => (blackboard: AIBlackboard) => {
     return !!AI.CanActivateAction(action)
+}
+
+/**
+ *
+ */
+export const Predicate_CanActivateUserAction = (userAction) => (blackboard: AIBlackboard) => {
+    if (!CURRENT_AI_CONFIG.enableUserAction) return false
+
+    return !!AI.CanActivateAction(userAction)
 }
