5a3e61975aaf6e3e9620e2b038d2d92841444ac2
Minsoo Kim <kim.aus@outlook.com>
2022-08-24 13:34:37 +0800

    Updated hacked script

--- Content/Scripts/hacked-mech-ai/src/tasks/focus/BTT_ClearFocalPoint.ts
+++ Content/Scripts/hacked-mech-ai/src/tasks/focus/BTT_ClearFocalPoint.ts
@@ -1,11 +1,19 @@
+import { AIBlackboard } from "@blackboards/blackboard"
 import { SUCCESS, Task } from "behaviortree"
-import { AI } from "../../index"
+import { AI } from "@root/index"
+import { ENABLE_DEBUGGING } from "@root/aiconfig"
 
 /**
  * Clears the focal point of AI set by {@link BTT_SetFocalPoint}.
  */
 export const BTT_ClearFocalPoint = new Task({
-    run: () => {
+    start: (blackboard: AIBlackboard) => {
+        if (ENABLE_DEBUGGING) {
+            console.log("BTT_ClearFocalPoint: Focus Cleared.")
+        }
+        return SUCCESS
+    },
+    run: (blackboard: AIBlackboard) => {
         AI.ClearFocus()
         return SUCCESS
     },
--- Content/Scripts/hacked-mech-ai/src/trees/BT_SearchHiddenLocation.ts
+++ Content/Scripts/hacked-mech-ai/src/trees/BT_SearchHiddenLocation.ts
@@ -1,12 +1,12 @@
 import { Sequence } from "behaviortree"
 import { Vector } from "types"
-import { EQSArgument, EQSQueryType } from "../../../types/enums"
-import { AIBlackboard } from "../blackboard"
-import { BTT_QuerySetArgumentVector } from "../tasks/environment/BTT_QuerySetArgument"
-import { BTT_MoveTo } from "../tasks/movement/BTT_MoveTo"
-import { BTT_RunEQSQuery } from "../tasks/environment/BTT_RunEQSQuery"
-import { BTT_SetValue } from "../tasks/BTT_SetValue"
-import { BTT_SetFocalPoint } from "../tasks/focus/BTT_SetFocalPoint"
+import { EQSArgument, EQSQueryType } from "enums"
+import { AIBlackboard } from "@blackboards/blackboard"
+import { BTT_QuerySetArgumentVector } from "@tasks/environment/BTT_QuerySetArgument"
+import { BTT_MoveTo } from "@tasks/movement/BTT_MoveTo"
+import { BTT_RunEQSQuery } from "@tasks/environment/BTT_RunEQSQuery"
+import { BTT_SetValue } from "@tasks/BTT_SetValue"
+import { BTT_SetFocalPoint } from "@tasks/focus/BTT_SetFocalPoint"
 
 /**
  * Behavior for searching possible hidden location based on the provided search location.
