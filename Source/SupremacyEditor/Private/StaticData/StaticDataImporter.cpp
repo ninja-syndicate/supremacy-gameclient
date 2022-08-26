c5e1a66a1dbbefc54ae85f8b265ceb8e5c9e94e9
lordmortis <lordmortis@gmail.com>
2022-08-26 08:41:19 +0800

    Static Data Import - initial framework and factions (#502)
    * Basic modules stuff
    
    * Can now bring up the directory dialog!
    
    * start working on structs
    
    * Set path functionality working, basic UI
    
    * working CSV line parser
    
    * validate headers are correct
    
    * Properties now visible in unreal, make dataasset
    
    * get a reference to the asset in the editor
    
    * data is importing!!!
    
    * Importing!

---
+++ Source/SupremacyEditor/Private/StaticData/StaticDataImporter.cpp
@@ -0,0 +1,105 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StaticDataImporter.h"
+#include "SupremacyEditorModule.h"
+#include "DesktopPlatformModule.h"
+
+UStaticDataImporter::UStaticDataImporter()
+{
+	FactionImporter = new StaticDataImporter::Faction();
+	ImportPath = TEXT("");
+	DesktopPlatform = FDesktopPlatformModule::Get();
+	if (DesktopPlatform == nullptr)
+	{
+		UE_LOG(LogSupremacyEditor, Error, TEXT("Unable to load desktop platform! This is a fatal error!"))
+		return;
+	}
+}
+
+UStaticDataImporter::~UStaticDataImporter()
+{
+	delete(FactionImporter);
+}
+
+bool UStaticDataImporter::SetImportDirectory()
+{
+	Ready = false;
+	if (DesktopPlatform == nullptr)
+	{
+		LogError("Unable to load desktop platform! Can't set import directory");
+		return false;
+	}
+
+	FString outFolder;
+	if (!DesktopPlatform->OpenDirectoryDialog(nullptr, "Select Static Directory", "..", outFolder))
+	{
+		LogWarning("No Folder selected");
+		return false;
+	}
+
+	if (!IFileManager::Get().DirectoryExists(*outFolder))
+	{
+		LogWarning(FString::Printf(TEXT("No Directory found at %s"), *outFolder));
+		return false;
+	}
+
+	FactionImporter->SetDirectory(outFolder);
+	if (!FactionImporter->Valid())
+	{
+		LogWarning(FString::Printf(TEXT("Invalid Static Data found at %s"), *outFolder));
+		LogError(FactionImporter->GetErrorReason());
+		return false;
+	}
+
+	Ready = true;
+	ImportPath = outFolder;
+	return true;
+}
+
+bool UStaticDataImporter::IsReady()
+{
+	return Ready;
+}
+
+bool UStaticDataImporter::UpdateAsset(UStaticData* asset)
+{
+	if (ImportPath.Len() == 0)
+	{
+		LogError("Import Directory not set");
+		return false;
+	}
+
+	if (!IFileManager::Get().DirectoryExists(*ImportPath))
+	{
+		LogWarning(FString::Printf(TEXT("No Directory found at %s"), *ImportPath));
+		return false;
+	}
+
+	UKismetSystemLibrary::BeginTransaction("StaticDataImporter", FText::FromString("Data Import"), asset);
+	UKismetSystemLibrary::TransactObject(asset);
+	FactionImporter->ImportAndUpdate(asset);
+	asset->Modify(true);
+	UKismetSystemLibrary::EndTransaction();
+	LogMessage("Import Succeeded!");
+	return true;
+}
+
+void UStaticDataImporter::LogError(const FString Text) const
+{
+	OnLogMessage.Broadcast(FString::Printf(TEXT("%ls\n"), *Text));
+	UE_LOG(LogSupremacyEditor, Error, TEXT("%s"), *Text);
+}
+
+void UStaticDataImporter::LogWarning(const FString Text) const
+{
+	OnLogMessage.Broadcast(FString::Printf(TEXT("%ls\n"), *Text));
+	UE_LOG(LogSupremacyEditor, Warning, TEXT("%s"), *Text);
+}
+
+void UStaticDataImporter::LogMessage(const FString Text) const
+{
+	OnLogMessage.Broadcast(FString::Printf(TEXT("%ls\n"), *Text));
+	UE_LOG(LogSupremacyEditor, Log, TEXT("%s"), *Text);
+}
+
---
+++ Source/SupremacyEditor/Private/Utils/CSVImporter.cpp
@@ -0,0 +1,108 @@
+﻿#include "CSVImporter.h"
+
+#include "Misc/FileHelper.h"
+
+bool Utils::CSVImporter::SetFileName(FString Filename)
+{
+	if (Filename == CurrentFilename) return true;
+	dataLoaded = false;
+	CurrentFilename = Filename;
+	return true;
+}
+
+bool Utils::CSVImporter::GetHeader(TArray<FString>& Result)
+{
+	if (!LoadData()) return false;
+
+	if (FileData.Num() == 0) return false;
+	return ParseLine(Result, FileData[0]);
+}
+
+void Utils::CSVImporter::Reset()
+{
+	currentIndex = 1;
+}
+
+bool Utils::CSVImporter::GetNextDataRow(TArray<FString>& Result)
+{
+	if (!LoadData()) return false;
+	if (currentIndex >= FileData.Num()) return false;
+
+	const bool result = ParseLine(Result, FileData[currentIndex]);
+	if (result) currentIndex++;
+	return result;
+}
+
+bool Utils::CSVImporter::ParseLine(TArray<FString>& Result, FString Line)
+{
+	Result.Empty();
+	bool inQuote = false;
+	bool startOfField = true;
+	FString currentValue;
+	while(Line.Len() > 0)
+	{
+		TCHAR token = Line[0];
+		if (token == '"')
+		{
+			if (startOfField)
+			{
+				inQuote = true;
+				Line.RemoveAt(0, 1);
+				continue;
+			}
+
+			if (inQuote)
+			{
+				inQuote = false;
+				Line.RemoveAt(0, 1);
+				continue;
+			}
+		}
+
+		if (token == '\\' && Line.Len() > 1)
+		{
+			if (Line[1] == '"')
+			{
+				currentValue.AppendChar('\"');
+				Line.RemoveAt(0, 2);
+				continue;
+			}
+
+			if (Line[1] == '\\')
+			{
+				currentValue.AppendChar('\\');
+				Line.RemoveAt(0, 2);
+				continue;
+			}
+		}
+
+		if (!inQuote && token == ',')
+		{
+			Result.Push(currentValue);
+			currentValue.Empty();
+			startOfField = true;
+			Line.RemoveAt(0, 1);
+			continue;
+		}
+
+		currentValue.AppendChar(token);
+		startOfField = false;
+		Line.RemoveAt(0, 1);
+	}
+
+	if (currentValue.Len() > 0) Result.Push(currentValue);
+
+	return true;
+}
+
+bool Utils::CSVImporter::LoadData()
+{
+	if (dataLoaded) return true;
+	if (!FFileHelper::LoadFileToStringArray(FileData, *CurrentFilename))
+	{
+		return false;
+	}
+	
+	dataLoaded = true;
+	return true;
+}
