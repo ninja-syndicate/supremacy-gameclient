40650b30f6d725b4dee97d9e202f5a4f57c2fae9
Minsoo Kim <kim.aus@outlook.com>
2022-08-30 18:42:56 +0800

    Re-named

--- Content/Scripts/default-mech-ai/src/predicates/Predicate_HasLowShield.ts
+++ Content/Scripts/default-mech-ai/src/predicates/Predicate_HasLowShield.ts
@@ -7,7 +7,9 @@ import { CURRENT_AI_CONFIG } from "@root/aiconfig"
  * @param blackboard
  * @returns true if the percentage of current shield to shield max is <= {@link CURRENT_AI_CONFIG.lowShieldThreshold} and false otherwise
  */
-export const HasLowShield = (blackboard: AIBlackboard) => {
-    let self = blackboard.input.self
+export const Predicate_HasLowShield = (blackboard: AIBlackboard) => {
+    const self = blackboard.input.self
+    if (self.shieldMax <= 0) return false
+
     return self.shield / self.shieldMax <= CURRENT_AI_CONFIG.lowShieldThreshold
 }
