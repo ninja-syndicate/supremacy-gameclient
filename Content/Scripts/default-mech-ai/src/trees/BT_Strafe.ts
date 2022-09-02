d663a68e6a1b56b295283148a6e41c7f5a2dc659
Minsoo Kim <kim.aus@outlook.com>
2022-09-02 15:10:02 +0800

    Increased minimum grid size

--- Content/Scripts/default-mech-ai/src/trees/BT_Strafe.ts
+++ Content/Scripts/default-mech-ai/src/trees/BT_Strafe.ts
@@ -17,7 +17,7 @@ import { BT_MoveByDistanceToTarget } from "@trees/movement/BT_MovementMode"
 export const BT_Strafe = new Sequence({
     nodes: [
         BTT_QuerySetArgumentString(EQSQueryType.Strafe, EQSArgument.TargetHash, (blackboard: AIBlackboard) => blackboard.target.hash),
-        BTT_QuerySetArgumentFloat(EQSQueryType.Strafe, EQSArgument.GridSize, (blackboard: AIBlackboard) => Math.max(10000, blackboard.idealEngagementRange)),
+        BTT_QuerySetArgumentFloat(EQSQueryType.Strafe, EQSArgument.GridSize, (blackboard: AIBlackboard) => Math.max(20000, blackboard.idealEngagementRange)),
         BTT_QuerySetArgumentFloat(EQSQueryType.Strafe, EQSArgument.MinDistanceToSelf, (blackboard: AIBlackboard) => 3000),
         BTT_QuerySetArgumentFloat(EQSQueryType.Strafe, EQSArgument.MinDistanceToTarget, (blackboard: AIBlackboard) => 0),
         BTT_QuerySetArgumentFloat(EQSQueryType.Strafe, EQSArgument.MaxDistanceToTarget, (blackboard: AIBlackboard) => blackboard.idealEngagementRange),
