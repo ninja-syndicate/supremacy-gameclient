bb3a5b2a6b3a2ebb22e92bcc659855e9dd27293e
Torrunt <me@torrunt.net>
2022-09-16 09:38:06 +0800

    Fixed casing + anim bp recoil setup

--- Source/Supremacy/Public/Types/WeaponStruct.h
+++ Source/Supremacy/Public/Types/WeaponStruct.h
@@ -50,7 +50,7 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ToolTip="For AI reference."))
 	int Optimal_Range;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ToolTip = "For recoil on the Mech's arms."))
-    float Recoil_Force;
+	float Recoil_Force;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float Power_Cost;
@@ -66,7 +66,7 @@ public:
 	                 Projectile_Amount(0),
 	                 Charge_Time(0),
 	                 Is_Arced(false), Optimal_Range(0),
-					 Recoil_Force(0),
+	                 Recoil_Force(0),
 	                 Power_Cost(0), Power_Instance_Drain(false)
 	{
 	}
