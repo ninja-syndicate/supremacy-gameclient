2a0dc62d2933526b8a8f0879bbc3efd93661605d
Minsoo Kim <kim.aus@outlook.com>
2022-10-04 12:35:55 +0800

    C++ Damager, dot actor and params struct

---
+++ Source/Supremacy/Public/Core/Damage/DotDamageParams.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DotDamageParams.generated.h"
+
+USTRUCT(BlueprintType)
+struct FDotDamageParams {
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dot Damage Parameters")
+	float TickDamage = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dot Damage Parameters")
+	float TickDuration = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dot Damage Parameters")
+	float DamageRadius = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dot Damage Parameters")
+	bool bEnableFriendlyFire = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dot Damage Parameters")
+	int	MaxTicks = 0;
+};
