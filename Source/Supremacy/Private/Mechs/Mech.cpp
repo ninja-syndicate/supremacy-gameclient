6a8cabd1a55088848688c73618fc5cb5bbae609a
kimaust <59196774+kimaust@users.noreply.github.com>
2022-10-20 17:09:16 +0800

    AI Melee Fix (#596)
    * Changed agent radius cale modifier
    
    * Moved weapon array to c++ class, fixed incorrect condition check, and sort weapons on war machine struct creation
    
    * Moved to use slots instead
    
    * Re-saved after moving weapon to C++ Mech
    
    * Removed old weapons array

--- Source/Supremacy/Private/Mechs/Mech.cpp
+++ Source/Supremacy/Private/Mechs/Mech.cpp
@@ -3,13 +3,17 @@
 #include "Kismet/KismetSystemLibrary.h"
 #include "Net/UnrealNetwork.h"
 
+#include "Weapons/Weapon.h"
+
 // Sets default values
 AMech::AMech() {}
 
 void AMech::GetLifetimeReplicatedProps( TArray< FLifetimeProperty > & OutLifetimeProps ) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps); 
-	DOREPLIFETIME_CONDITION( AMech, WarMachineStruct, COND_InitialOnly );
+
+	DOREPLIFETIME_CONDITION(AMech, WarMachineStruct, COND_InitialOnly);
+	DOREPLIFETIME_CONDITION(AMech, Weapons, COND_None);
 }
 
 void AMech::OnRep_SetWarMachineStruct()
@@ -17,3 +21,8 @@ void AMech::OnRep_SetWarMachineStruct()
 	Setup();
 }
 
+AWeapon* AMech::GetWeaponBySlot_Implementation(int SlotIndex)
+{
+	// Assumes the `Weapons` array is sorted by slot.
+	return SlotIndex >= 0 && SlotIndex < Weapons.Num() ? Weapons[SlotIndex] : nullptr;
+}
