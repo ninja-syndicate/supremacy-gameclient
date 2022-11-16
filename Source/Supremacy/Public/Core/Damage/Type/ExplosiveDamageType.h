769ff7eded6a00e80c3294b63dfabf9649dccd3a
Minsoo Kim <kim.aus@outlook.com>
2022-11-16 13:17:04 +0800

    C++ damage types

---
+++ Source/Supremacy/Public/Core/Damage/Type/ExplosiveDamageType.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "ExplosiveDamageType.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPREMACY_API UExplosiveDamageType : public UDamageType
+{
+	GENERATED_BODY()
+	
+public:
+	UExplosiveDamageType();
+};
