56b5725698c1433ba21114480009aab783e942f6
Torrunt <me@torrunt.net>
2022-11-16 12:12:48 +0800

    Pack ammo count fix + skip unchanged

--- Source/Supremacy/Public/BPFL_Helpers.h
+++ Source/Supremacy/Public/BPFL_Helpers.h
@@ -29,8 +29,8 @@ public:
 	static void PackWarMachineUpdate(const uint8 Number, const int X, const int Y, const int Yaw, const int Health, const int Shield, const int Energy, 
 									 const TArray<bool> DiffArray, TArray<uint8> &Bytes);
 
-	UFUNCTION(BlueprintPure, Category = "Network", DisplayName = "Pack WarMachine Weapon Updates", meta = (Keywords = "Net Message Bytes Byte Array Binary"))
-	static void PackWarMachineWeaponUpdates(const uint8 UpdatedWeaponCount, const TArray<uint8>& WeaponIndices, const TArray<int>& WeaponAmmoCounts, TArray<uint8>& Bytes);
+	UFUNCTION(BlueprintPure, Category = "Network", DisplayName = "Pack WarMachine Weapon Updates", meta = (Keywords = "Net Message Bytes Byte Array Binary", ToolTip="Packs weapon ammo counts, skipped if previous total ammo = current total ammo"))
+	static void PackWarMachineWeaponUpdates(UObject* WarMachine, const int PreviousTotalAmmo, TArray<uint8>& Bytes, int& TotalAmmo);
 	
 	/** Converts a String to an Array of bytes */
 	UFUNCTION(BlueprintPure, Category = "Helpers", DisplayName = "Convert String To Bytes")
