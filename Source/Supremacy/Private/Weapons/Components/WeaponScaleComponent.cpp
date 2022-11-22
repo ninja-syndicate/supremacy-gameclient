be7f1e23ca776f1f12fe1707a319cf9e6af62006
Minsoo Kim <kim.aus@outlook.com>
2022-11-22 17:52:59 +0800

    WIP C++ Weapon scale component

--- Source/Supremacy/Private/Weapons/Components/WeaponScaleComponent.cpp
+++ Source/Supremacy/Private/Weapons/Components/WeaponScaleComponent.cpp
@@ -3,6 +3,9 @@
 
 #include "Weapons/Components/WeaponScaleComponent.h"
 
+#include "Logger/SupremacyLogTypes.h"
+#include "Weapons/WeaponizedInterface.h"
+
 // Sets default values for this component's properties
 UWeaponScaleComponent::UWeaponScaleComponent()
 {
@@ -11,32 +14,44 @@ UWeaponScaleComponent::UWeaponScaleComponent()
 	PrimaryComponentTick.bCanEverTick = false;
 }
 
-
 // Called when the game starts
 void UWeaponScaleComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	/*
 	if (!GetOwner())
 	{
-		UE_LOG(Log.., Error, TEXT(""));
+		UE_LOG(LogComponent, Error, TEXT("UWeaponScaleComponent: Invalid owner."));
 		return;
 	}
 
-	if (!GetOwner()->GetInstigator())
+	APawn* Instigator = GetOwner()->GetInstigator();
+	if (!Instigator)
 	{
-
+		UE_LOG(LogComponent, Error, TEXT("UWeaponScaleComponent: Invalid instigator."));
+		return;
 	}
-	*/
-}
 
+	if (!Instigator->Implements<UWeaponizedInterface>())
+	{
+		UE_LOG(LogComponent, Error, TEXT("UWeaponScaleComponent: Instigator does not imlpement UWeaponizedInterface!"));
+		return;
+	}
+
+	TArray<UStaticMeshComponent*> StaticComps;
+	GetOwner()->GetComponents<UStaticMeshComponent>(StaticComps, true);
+
+	const float WeaponBaseScale = IWeaponizedInterface::Execute_GetWeaponBaseScale(Instigator);
+	for (UStaticMeshComponent* StaticComp : StaticComps)
+	{
+		//@todo - need to strict scale to just static mesh comp, not Niagara vfx.
+		const FVector NewRelativeScale = StaticComp->GetRelativeScale3D() * WeaponBaseScale;
+		StaticComp->SetRelativeScale3D(NewRelativeScale);
+	}
+}
 
 // Called every frame
 void UWeaponScaleComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
-
