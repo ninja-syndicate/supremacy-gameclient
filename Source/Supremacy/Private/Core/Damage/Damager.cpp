409ac4cccced8ce759fa15499ae6299653bb4673
michaelsupremacygame <117133027+michaelsupremacygame@users.noreply.github.com>
2022-11-28 17:25:27 +0800

    Optimization pass (#651)
    * reduce decal max lifetime to 10 seconds from infinite
    
    * turn on pooling for one-shot niagara systems
    
    * increase decal lifetime from 10 to 20 seconds
    
    * enable pooling for some more one-shot niagara systems
    
    * shortened particle lifetimes for some particle effects, especially those that spawn frequently
    
    * don't tick damager, as it and it's child AC_Damager doesn't do anything with tick
    
    * Fixed conflict

--- Source/Supremacy/Private/Core/Damage/Damager.cpp
+++ Source/Supremacy/Private/Core/Damage/Damager.cpp
@@ -13,7 +13,7 @@ UDamager::UDamager()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 
