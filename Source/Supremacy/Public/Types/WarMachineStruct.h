f4d7487e1bc04c46872f1800cd1c2331f0b207a5
Thomas Tetlaw <thomas@tetlaw.id.au>
2022-11-29 11:49:47 +0800

    set shield recharge delay from warmachinestruct (#654)
    * set shield recharge delay from warmachinestruct
    
    * Undid test code + add 0 recharge delay check
    
    If recharge delay is <= 0, default to 5.
    
    Co-authored-by: Torrunt <me@torrunt.net>

--- Source/Supremacy/Public/Types/WarMachineStruct.h
+++ Source/Supremacy/Public/Types/WarMachineStruct.h
@@ -99,7 +99,8 @@ public:
 	int Shield_Max;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float Shield_Recharge_Rate;
-
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float Shield_Recharge_Delay;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int Speed;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
@@ -163,6 +164,8 @@ public:
 	int ShieldMax;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float ShieldRechargeRate;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ShieldRechargeDelay;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int Speed;
@@ -205,6 +208,7 @@ public:
 		HealthMax(WarMachine.Health_Max),
 		ShieldMax(WarMachine.Shield_Max),
 		ShieldRechargeRate(WarMachine.Shield_Recharge_Rate),
+		ShieldRechargeDelay(WarMachine.Shield_Recharge_Delay),
 
 		Speed(WarMachine.Speed),
 		WalkSpeedModifier(WarMachine.Walk_Speed_Modifier),
